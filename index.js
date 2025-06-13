0a918fbf544616e78c3f12654b55a6b639e6f0a1
EdibleHerald <142949302+EdibleHerald@users.noreply.github.com>
2025-06-13 08:55:55 -0400

    Added functionality to filter through games that have been funded or not fully funded

--- index.js
+++ index.js
@@ -107,10 +107,11 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
-
+    let filteredGames = GAMES_JSON.filter( (games => {
+        return games.pledged < games.goal;
+    }));
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(filteredGames);
 }
 
 // show only games that are fully funded
@@ -118,18 +119,20 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let filteredGames = GAMES_JSON.filter( (games) => {
+        return games.pledged >= games.goal;
+    })
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(filteredGames);
 }
 
 // show all games
 function showAllGames() {
     deleteChildElements(gamesContainer);
-
+    
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -138,7 +141,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click",filterUnfundedOnly);
+fundedBtn.addEventListener("click",filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
